94272674be883c7dedd1c7d713225c66e2b4870f
cha2y0ung <102947253+cha2y0ung@users.noreply.github.com>
2022-07-30 05:42:23 +0900

    Add files via upload

--- ugait/apps/views.py
+++ ugait/apps/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.db import models
+from django.shortcuts import *
+from .models import *
+from forms import *
 # Create your views here.
+
+
+def findaguideview(request):
+    location = User.location
+    if location == 'seoul':
+        tourtheme1 = request.POST['tourtheme']
+        if tourtheme1 == Guide.tourtheme:
+            return render('match.html')
+
+
+def guidematchedview(request):
+    return render('match_success.html')
